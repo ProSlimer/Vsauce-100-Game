feb28aaf22ede5a855e2667b8e4018550913c4fd
ProSlimer <76175042+ProSlimer@users.noreply.github.com>
2023-10-17 17:27:38 -0500

    Update vsauce.py

--- vsauce.py
+++ vsauce.py
@@ -14,37 +14,36 @@
 
 import random
 
-#Initiate all used variables to 0
+# Initiate all used variables to 0
 number = 0
 useradd = 0
+cpuadd = 0
 turn = 0
-level = 0
+difficulty = 0
 
-def start(level): #The starting "menu" for the game. Includes difficulty setting
-        level = 0
-        print("Welcome to The 100 Game!")
-        level = int(input("What difficulty would you like to play? (1-3)\n"))
-        level = int(level)
-        if level > 3 or level < 1:
-                print("That is not a valid difficulty option, please try again.\n")
-                level = 0
-                start(level)
-        return level
+def start(difficulty):
+    difficulty = 0
+    print("Welcome to The 100 Game!")
+    difficulty = int(input("What difficulty would you like to play? (1-3)\n"))
+    difficulty = int(difficulty)
+    if difficulty > 3 or difficulty < 1:
+        print("That is not a valid difficulty option, please try again.\n")
+        difficulty = 0
+        start(difficulty)
+    return difficulty
 
 def userturn(number):
-               useradd = int(input("Please enter your number. (1-10)\n"))
-               #Check for invalid inputs
-               if useradd > 10 or useradd < 1:
-                       print("That number isn't between 1 and 10, please enter a number between 1 and 10")
-                       userturn(number)
-               elif useradd + number > 100:
-                       print("That number is too high, please enter a smaller number so you don't pass 100")
-                       userturn(number)
-                #IF valid, add the input number to the total number
-               else:
-                       number += useradd
-                       print(f"The new number is {number}.\n")
-               return useradd, number #Returns the "useradd" and "number" variables
+    useradd = int(input("Please enter your number. (1-10)\n"))
+    if useradd > 10 or useradd < 1:
+        print("That number isn't between 1 and 10, please enter a number between 1 and 10")
+        return userturn(number)  # Return the result of the recursive call
+    elif useradd + number > 100:
+        print("That number is too high, please enter a smaller number so you don't pass 100")
+        return userturn(number)  # Return the result of the recursive call
+    else:
+        number += useradd
+        print(f"The new number is {number}.\n")
+    return useradd, number
 
 def computerrandom(number):
     cpuadd = random.randint(1, 10)
@@ -55,48 +54,48 @@ def computerrandom(number):
         print(f"The computer chose {cpuadd}, the new number is {number}.")
     return number
 
-
-def computerturn(useradd, level, number):
-    print("DEBUG: `USERADD` after being passed to `COMPUTERTURN` function:",useradd) #The `useradd` variable does not seem to be passed properly. 
-    if level == 1:
+def computerturn(useradd, difficulty, number):
+    print("DEBUG: `USERADD` after being passed to `COMPUTERTURN` function:", useradd)
+    if difficulty == 1:
         number = computerrandom(number)
-    elif level == 2:
+    elif difficulty == 2:
         if number < 50:
             number = computerrandom(number)
         else:
             cpuadd = 11 - useradd
             number += cpuadd
             print(f"The computer chose {cpuadd} the new number is {number}.")
-    elif level == 3:
+    elif difficulty == 3:
         if useradd == 0:
-               computerrandom(number)
+            computerrandom(number)
         else:
-                cpuadd = 11 - useradd
-                number += cpuadd
-                print(f"The computer chose {cpuadd} the new number is {number}.")
+            cpuadd = 11 - useradd
+            number += cpuadd
+            print(f"The computer chose {cpuadd} the new number is {number}.")
     else:
-        print (f"Level variable is invalid? The current level variable is:{level}. Fix something.") #Error message for the case that the "level" variable is not 1-3. This has happened a few times somehow.
+        print(f"difficulty variable is invalid? The current difficulty variable is:{difficulty}. Fix something.")
         quit()
     return number
 
+difficulty = start(difficulty)
+turn = random.randint(0, 1)
 
-level = start(level)
-turn = random.randint(0,1)
 if turn == 0:
-        print("You get to go first!")
+    print("You get to go first!")
 else:
-        print ("The computer will go first.")
+    print("The computer will go first.")
 
 while number < 100:
-        if turn == 0:
-                useeradd, number = userturn(number)
-                turn = 1
-        else:
-                number = computerturn(useradd, level, number)
-                turn = 0
+    if turn == 0:
+        useradd, number = userturn(number)
+        turn = 1
+    else:
+        number = computerturn(useradd, difficulty, number)
+        turn = 0
+
 if turn == 0:
-        print("The computer wins, better luck next time!")
+    print("The computer wins, better luck next time!")
 elif turn == 1:
-        print("You win! Great job!")
+    print("You win! Great job!")
 else:
-        print("The turn variable is invalid. How did you do that? Turn:",turn)
+    print("The turn variable is invalid. How did you do that? Turn:", turn)
